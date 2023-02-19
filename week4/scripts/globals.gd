ba761295892b4006d0e39b1c1debacf917a49d5a
Adrian <an3015@nyu.edu>
2023-02-19 01:44:37 -0500

    Market creation

---
+++ week4/scripts/globals.gd
@@ -0,0 +1,17 @@
+"""
+This file is used to declare global variables. 
+
+To declare a global variable:
+	var variable_name
+
+To use this global variable in another script:
+	global.variable_name
+
+This file is auto-loaded, you don't have to do anything. You can check this out in 
+Project > Project Setting > Autoload
+"""
+
+extends Node
+
+# Player's balance
+var balance
---
+++ week4/scripts/market.gd
@@ -0,0 +1,34 @@
+# Naming class
+class_name Market extends Node
+
+# Member Variables
+var store = {}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	load_json_file()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func load_json_file():
+	"""
+	Populates the store with items from JSON file
+	"""
+	
+	var file = File.new()
+	file.open("res://assets/prices.json", file.READ)
+	store = parse_json(file.get_as_text())
+	file.close()
+	
+
+func purchase_item(category, item):
+	"""
+	Customer buys an item from the store
+	"""
+	
+	global.balance += store[category][item]
